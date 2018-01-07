e727acac9ee061146176282c30642542a5bac378
Bernardo <programming@wandrell.com>
2018-01-07 13:29:18 +0100

    Added placeholder test

---
+++ tests/test_placeholder.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+"""
+Placeholder tests.
+"""
+
+__author__ = '{{ cookiecutter.developer_name }}'
+__license__ = 'MIT'
+
+
+class TestPlaceholder(unittest.TestCase):
+    """
+    Placeholder for a test case.
+    """
+
+    def setUp(self):
+        """
+        Here the tests environment would be prepared.
+        """
+        pass
+
+    def test_true(self):
+        """
+        A placeholder test which is always true.
+        """
+        self.assertTrue(True)
