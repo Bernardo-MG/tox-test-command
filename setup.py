c4b49b7b1b512e10ac61d9faf335819e02fefca4
Bernardo <programming@bernardomg.com>
2021-03-29 21:11:59 +0200

    Updated versions support

--- setup.py
+++ setup.py
@@ -86,10 +86,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
     ],
     long_description=read('README.rst'),
     install_requires=[
